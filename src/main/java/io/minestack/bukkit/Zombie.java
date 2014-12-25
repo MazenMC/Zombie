b1de6f6da7b346a4719137292cb58131af1771b3
Jodie Belgrave <rmb1993@gmail.com>
2014-12-25 16:25:26 -0500

    add publish for serverStart and serverStop

--- src/main/java/io/minestack/bukkit/Zombie.java
+++ src/main/java/io/minestack/bukkit/Zombie.java
@@ -7,10 +7,13 @@ import com.github.dockerjava.core.DockerClientBuilder;
 import com.mongodb.ServerAddress;
 import com.rabbitmq.client.Address;
 import io.minestack.doublechest.DoubleChest;
+import io.minestack.doublechest.databases.rabbitmq.pubsub.PubSubPublisher;
 import io.minestack.doublechest.model.server.Server;
 import org.bson.types.ObjectId;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -41,7 +44,7 @@ public class Zombie extends JavaPlugin {
             try {
                 addresses.add(new ServerAddress(split[0], port));
             } catch (UnknownHostException e) {
-                getLogger().log(Level.SEVERE, "Threw a UnknownHostException in Redstone::main, full stack trace follows: ", e);
+                getLogger().log(Level.SEVERE, "Threw a UnknownHostException in Zombie::onEnable, full stack trace follows: ", e);
             }
         }
         if (System.getenv("mongo_username") == null) {
@@ -93,6 +96,8 @@ public class Zombie extends JavaPlugin {
                 return;
             }
 
+            boolean newServer = false;
+
             if (server.getPort() == -1) {
                 DockerClient dockerClient = DockerClientBuilder.getInstance("http://" + server.getNode().getPrivateAddress() + ":4243").build();
                 InspectContainerResponse inspectContainerResponse = dockerClient.inspectContainerCmd(server.getContainerId()).exec();
@@ -100,8 +105,22 @@ public class Zombie extends JavaPlugin {
                     if (exposedPort.getPort() == 25565) {
                         int hostPort = inspectContainerResponse.getNetworkSettings().getPorts().getBindings().get(exposedPort)[0].getHostPort();
                         server.setPort(hostPort);
+                        newServer = true;
                         break;
                     }
+                }
+            }
+
+            if (newServer == true) {
+                try {
+                    PubSubPublisher pubSubPublisher = new PubSubPublisher(DoubleChest.INSTANCE.getRabbitMQDatabase(), "serverStart");
+
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("server", server.getId().toString());
+
+                    pubSubPublisher.publish(jsonObject);
+                } catch (IOException e) {
+                    getLogger().log(Level.SEVERE, "Threw a IOException in Zombie::onEnable::asyncTask, full stack trace follows: ", e);
                 }
             }
 
@@ -117,6 +136,17 @@ public class Zombie extends JavaPlugin {
         Server server = DoubleChest.INSTANCE.getMongoDatabase().getServerRepository().getModel(new ObjectId(System.getenv("server_id")));
         server.setUpdated_at(new Date(0));
         DoubleChest.INSTANCE.getMongoDatabase().getServerRepository().saveModel(server);
+
+        try {
+            PubSubPublisher pubSubPublisher = new PubSubPublisher(DoubleChest.INSTANCE.getRabbitMQDatabase(), "serverStop");
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("server", server.getId().toString());
+
+            pubSubPublisher.publish(jsonObject);
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Threw a IOException in Zombie::onDisable, full stack trace follows: ", e);
+        }
     }
 
 }
